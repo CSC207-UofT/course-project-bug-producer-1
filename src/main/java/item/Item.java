fd9c6f1e95190b66eea60882b23782e8d20ef0cf
Xingru Ren <xingru.ren@mail.utoronto.ca>
2021-10-12 18:22:22 -0400

    item part by Jamie

--- src/main/java/item/Item.java
+++ src/main/java/item/Item.java
@@ -1,4 +1,29 @@
 package main.java.item;
+import main.java.item.Order;
+import main.java.item.Inventory;
+
+import java.util.HashMap;
 
 public class Item {
+    private String name;
+    private int ID;
+    private int capacity;
+    private int quantity;
+    public Item(String name, int ID, int capacity, int quantity){
+        this.name = name;
+        this.ID = ID;
+        this.capacity = capacity;
+        this.quantity = quantity;
+    }
+
+
+    public void update_quantity_in(int num){
+        this.quantity = this.quantity - num;
+    }
+
+    public void update_quantity_out(int num){
+        this.quantity = this.quantity - num;
+    }
+
+
 }
