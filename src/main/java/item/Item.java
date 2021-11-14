b13d57c55deaef49c5271dde6898e47a1d6ee25f
Xingru Ren <xingru.ren@mail.utoronto.ca>
2021-11-14 17:51:24 -0500

    adding java doc of Item

--- src/main/java/item/Item.java
+++ src/main/java/item/Item.java
@@ -3,12 +3,20 @@ import main.java.order.Order;
 import main.java.inventory.Inventory;
 
 import java.util.HashMap;
-
+/**
+ * This class is a entity class for items in inverntory . It is a parent class and subclasses are
+ * Apple, Banana, Beef and so on.
+ * @author Xingru Ren
+ */
 public class Item {
     private String name;
     private int ID;
     private int capacity;
     private int quantity;
+    /**
+     * This method is the initiator of class Item.
+     * It requires the name, ID, capacity and quantity of each item.
+     */
     public Item(String name, int ID, int capacity, int quantity){
         this.name = name;
         this.ID = ID;
@@ -16,23 +24,45 @@ public class Item {
         this.quantity = quantity;
     }
 
-
+    /**
+     * This method update the quantity of item by adding specific number to quantity.
+     * This method does not return anything.
+     */
     public void update_quantity_in(int num){
-        this.quantity = this.quantity - num;
+        this.quantity = this.quantity + num;
     }
 
+
+    /**
+     * This method update the quantity of item by subtracting specific number to quantity.
+     * This method does not return anything.
+     */
     public void update_quantity_out(int num){
         this.quantity = this.quantity - num;
     }
 
+
+    /**
+     * This method return the name of the item.
+     * This method does not require parameter.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * This method return the capacity of the item.
+     * This method does not require parameter.
+     */
     public int getCapacity() {
         return capacity;
     }
 
+
+    /**
+     * This method return the quantity of the item.
+     * This method does not require parameter.
+     */
     public int getQuantity() {
         return quantity;
     }
