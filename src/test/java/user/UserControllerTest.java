7d3635a3ff1f0c6fd955bbd8fcf0c13918f79a51
ReaganGen <reaganree.li@mail.utoronto.ca>
2021-11-15 17:05:50 -0500

    update of register

--- src/test/java/user/UserControllerTest.java
+++ src/test/java/user/UserControllerTest.java
@@ -25,13 +25,8 @@ public class UserControllerTest {
     @Test
     public void test_register(){
         Database database_S = new Database();
-        ArrayList<String> list = new ArrayList<>();
-        list.add("reagan.li@icloud.com");
-        list.add("12345");
-        list.add("1");
-        list.add("Customer");
-        database_S.database.put("Elva", list);
-        boolean actual = register("Elva", "0000", "0000", database_S);
+
+        boolean actual = register("Elva", "0000", database_S, "12345", "123.li@icloud.com");
         Assert.assertTrue(database_S.database.containsKey("Elva"));
         Assert.assertTrue(actual);
 
@@ -40,15 +35,10 @@ public class UserControllerTest {
     @Test
     public void test_register_2(){
         Database database_S = new Database();
-        ArrayList<String> list = new ArrayList<>();
-        list.add("reagan.li@icloud.com");
-        list.add("12345");
-        list.add("1");
-        list.add("Customer");
-        database_S.database.put("Elva", list);
-        boolean actual = register("Elva", "0000", "1111", database_S);
-        Assert.assertFalse(database_S.database.containsKey("Elva"));
-        Assert.assertFalse(actual);
+
+        boolean actual = register("Elva", "bug", database_S, "12345", "123.li@icloud.com");
+        Assert.assertTrue(database_S.database.containsKey("Elva"));
+        Assert.assertTrue(actual);
     }
 
 }
