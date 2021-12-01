5cf3dcaa0a40777221f738148926c8d94544387c
ReaganGen <reaganree.li@mail.utoronto.ca>
2021-12-01 15:38:14 -0500

    Modification done by Gen LI. Refactoring the "Generate message" part into the presenter, and make the controller be responsible for creating message entity. Also make the structure of the code more concise. I combined the SendMailUseCase_Admin.java and SendMailUseCase_Customer.java into one UseCase class. Apart from that, I combined the MessageForAdmin.java and MessageForCustomer.java into one Message_R class by move the message content part into the presenter. In this way, the controller is responsible for taking the command from UI to create a new message entity by calling UseCase and presenter is responsible for generating and showing the content of message for UI, which obeys the clean architecture and SOLID principle.

--- src/main/java/message/MessagePresenter.java
+++ src/main/java/message/MessagePresenter.java
@@ -1,19 +1,37 @@
 package main.java.message;
 
 
+
+
 /**
  * This class
  */
 public class MessagePresenter {
 
-    public void print_out_message(String input_from_core_codes){
-        System.out.println(input_from_core_codes);
+    public static void print_out_message_for_customer(Message_R message){
+        String content = "Dear Customer," + "\n" + "Your order has been placed successfully! We are trying our best " +
+                "to process your order !" + "\n" + "Order details:" + "\n" + "Order ID: " + message.get_order_id() + "\n" +
+                "Items in the order: " + message.get_item_list() + "\n" + "Order status: " + message.get_status() + "\n" +
+                "\n" + "Order date: " + message.get_order_date() + "\n" + "Message date: " + message.get_message_date() +
+                "Thank you for your patience! Have a nice day!";
+        System.out.println(content);
+    }
+
+
+    public static void print_out_message_for_Admin(Message_R message){
+        String content = "Dear Admin," + "\n" + "This order has been placed successfully by the customer!" +
+                 "\n" + "Order details:" + "\n" + "Order ID: " + message.get_order_id() + "\n" +
+                "Items in the order: " + message.get_item_list() + "\n" + "Order status: " + message.get_status() + "\n" +
+                "\n" + "Order date: " + message.get_order_date() + "\n" + "Message date: " + message.get_message_date();
+        System.out.println(content);
     }
 
     public void print_success_notification_email(){
         System.out.println("The email has been sent successfully! Thank you!");
     }
-}
 
+    
+}
+// move message body here.
 
 
