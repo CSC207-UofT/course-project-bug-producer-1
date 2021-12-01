04567a96cd126369c7f7b7307196eb12f7fce8ce
Walter <walter@huang.moe>
2021-12-01 15:13:59 -0500

    GUI update added NewOrderGUI shopping cart function delete item

--- src/main/java/item/Cabbage.java
+++
@@ -1,8 +0,0 @@
-package main.java.item;
-
-public class Cabbage extends Item{
-
-    public Cabbage(String name, int ID, int capacity, int quantity){
-        super(name, ID, capacity, quantity);
-    }
-}
