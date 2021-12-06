8deddce1ae43a4bd3e55816748837b291ceab300
elvashen <meilun.shen@mail.utoronto.ca>
2021-12-05 19:26:29 -0500

    Updating Application Business Rule

--- src/main/java/user/UseCase/Log_inUseCase.java
+++ src/main/java/user/UseCase/Log_inUseCase.java
@@ -1,27 +1,58 @@
 package main.java.user.UseCase;
 
-import main.java.user.Database;
+import main.java.user.Admin;
+import main.java.user.Customer;
+import main.java.user.User;
 
-/**
- * This class is used for the login of users.
- */
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class Log_inUseCase {
 
     /**
-     * This method is used for the log_in of users. It checks if the password entered by the user
-     * matches the password stored in thr database
-     * @param user_name The name of the user. A String.
-     * @param user_pwd  A string that represents the password of the user.
-     * @param database The database that store the information of user's name and passwords etc.
-     * @return return a boolean type result, if matches, return true, otherwise, return false.
+     * Verifies whether the password entered by the User matches the saved password of the User.
+     * @param name A string represents the entered username.
+     * @param password A string represents the entered password.
+     * @param user_input_secret_code A string contain secret code for differentiating user's type.
+     * @return A hashmap contains information of the current logged-in user.
      */
 
-    public static boolean log_in(String user_name, String user_pwd, Database database){
-        try
-        {
-            return database.database.get(user_name).get(1).equals(user_pwd);
-        }catch(NullPointerException e){return false;}
-
+    public static HashMap<Boolean, ArrayList<String>> login (String name, String password, String user_input_secret_code) throws IOException {
+        HashMap<Boolean, ArrayList<String>> result = new HashMap<>();
+        for (String[] registerUser : UserReadWriter.readUsers()) {
+            String username = registerUser[0];
+            String existPwd = registerUser[2];
+            if (name.equals(username)) {
+                if (password.equals(existPwd)){
+                    User currentU;
+                    if(user_input_secret_code.equals("asd")) {
+                        currentU = new Admin(name, password, "");
+                        ArrayList<String> helper = new ArrayList<>();
+                        helper.add(currentU.getName());
+                        helper.add(currentU.getType());
+                        helper.add(currentU.getUser_email());
+                        helper.add(currentU.getPassword());
+                        result.put(true, helper);
+                    }
+                    else {
+                        currentU = new Customer(name, password, "");
+                        ArrayList<String> helper = new ArrayList<>();
+                        helper.add(currentU.getName());
+                        helper.add(currentU.getType());
+                        helper.add(currentU.getUser_email());
+                        helper.add(currentU.getPassword());
+                        result.put(false, helper);
+                    }
+                }
+                result.put(false, new ArrayList<>());
+                return result;
+            }
+        }
+        result.put(false, new ArrayList<>());
+        return result;
     }
 }
+
+
