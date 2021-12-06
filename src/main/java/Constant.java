5c92988ecbae67f33b2d0c368532376bba3c8933
Walter <walter@huang.moe>
2021-12-06 01:13:09 -0500

    temp solution to uesrname

--- src/main/java/Constant.java
+++ src/main/java/Constant.java
@@ -3,18 +3,18 @@ package main.java;
 import main.java.user.User;
 
 public class Constant {
-    public static String currentUser = "";
+    public static String currentUsername = "";
     public static final String version = "1.3.1";
 
     public Constant(){
     }
 
-    public static void setCurrUser(String user){
-        currentUser = user;
+    public static void setCurrUsername(String user){
+        currentUsername = user;
     }
 
-    public static String getCurrUser(){
-        return currentUser;
+    public static String getCurrUsername(){
+        return currentUsername;
     }
 }
 
