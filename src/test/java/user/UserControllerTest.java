d00e8cd3e7e36c17b846942b27c1fd11a1174b69
elvashen <meilun.shen@mail.utoronto.ca>
2021-12-07 22:23:14 -0500

    Debugged Usr Controller test

--- src/test/java/user/UserControllerTest.java
+++ src/test/java/user/UserControllerTest.java
@@ -1,38 +1,56 @@
-package user;
+package java.user;
 
+import org.junit.Before;
 import user.UseCase.UserReadWriter;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 
 import static user.UseCase.RegisterUseCase.register;
 import static user.UserController.log_in;
 
 public class UserControllerTest {
-//    @Test
-//    public void test_log_in() throws IOException {
-//
-//        UserReadWriter.writeUsers("Elva", "reagan.li@icloud.com", "12345", "Customer");
-//        boolean actual = log_in("Elva", "12345");
-//        Assert.assertTrue(actual);
-//
-//    }
-//
-//
-//
-//    @Test
-//    public void test_register() throws IOException {
-//
-//        //The database file "userdatabase.csv" need to be cleared. It should be empty.
-//
-//        boolean actual = register("Elva", "345.li@icloud.com", "12345", "admin");
-//        Assert.assertTrue(actual);
-//
-//        //test for overlapping usernames
-//        boolean actual2 = register("Elva", "123.li@icloud.com", "12345", "customer");
-//        Assert.assertFalse(actual2);
-//    }
+
+    // In case you run test cases more than one time, the initializer before test cases can help to empty the userdatabse.csv.
+    // Otherwise, when running test cases twice without the initializer, the username "Reagan" will be registered twice and test cases will fail.
+
+
+    @Before
+    public void init() {
+        File csv = new File("userdatabase.csv");
+        if(csv.delete()){
+            System.out.println("CSV File Deleted Successfully!!");
+        }
+        else{System.out.println("Failed to Delete the CSV File!!");}
+    }
+
+
+
+    @Test
+    public void test_log_in() throws IOException {
+
+        UserReadWriter.writeUsers("Elva", "reagan.li@icloud.com", "12345", "Customer");
+        boolean actual = log_in("Elva", "12345");
+        Assert.assertTrue(actual);
+
+    }
+
+
+
+    @Test
+    public void test_register() throws IOException {
+
+        //The database file "userdatabase.csv" need to be cleared. It should be empty.
+
+        boolean actual = register("Reagan", "345.li@icloud.com", "12345", "admin");
+        Assert.assertTrue(actual);
+
+        //test for overlapping usernames
+        boolean actual2 = register("Reagan", "123.li@icloud.com", "12345", "customer");
+        Assert.assertFalse(actual2);
+    }
 
 
 }
