9733bdd7c041785d9661e82413dceddd42b445b9
ReaganGen <90350002+ReaganGen@users.noreply.github.com>
2021-12-07 23:15:41 -0500

    Add files via upload
    Update the SendMailUseCase

--- src/main/java/message/UseCase/SendMailUseCase.java
+++ src/main/java/message/UseCase/SendMailUseCase.java
@@ -1,4 +1,5 @@
-package message.UseCase;
+package message.useCase;
+
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -9,34 +10,37 @@ import java.util.Properties;
 /**
  * This class is a UseCase class, which is used for sending email via the application.
  * The content of the email is the content typed by the User in the sending email panel.
+ *
  * @author GEN LI (Reagan)
  */
 
 public class SendMailUseCase {
 
-    public static String SMTPHost = "smtp.gmail.com";
+    public static final String SMTPHost = "smtp.gmail.com";
 
 
     /**
      * This method is used for creating an email with sender and receiver. Specifically the sender account for admin is
      * "bugproducer.customer@gmail.com" and its password is "Bugproducer123". The receiver account is typed by the user.
-     * @param session_R Session object for this email, which represents a mail session.
-     * @param senderMail The email address of the sender.
+     *
+     * @param session_R    Session object for this email, which represents a mail session.
+     * @param senderMail   The email address of the sender.
      * @param receiverMail The email address of the receiver.
-     * @param user_input The user's input, which is used as the content of th email.
+     * @param user_input   The user's input, which is used as the content of th email.
      * @return message_R an email with needed information.
      */
 
     public static MimeMessage createMail_customer(Session session_R, String senderMail, String receiverMail, String user_input) {
-        try{MimeMessage message_R = new MimeMessage(session_R);
-        message_R.setFrom(new InternetAddress(senderMail, "Customer", "UTF-8"));
-        message_R.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiverMail, "Bugproducer", "UTF-8"));
-        message_R.setSubject("Message from Customer", "UTF-8");
-        message_R.setContent(user_input, "text/html;charset=UTF-8");
-        message_R.setSentDate(new Date());
-        message_R.saveChanges();
-        return message_R;}
-        catch(Exception ignored){
+        try {
+            MimeMessage message_R = new MimeMessage(session_R);
+            message_R.setFrom(new InternetAddress(senderMail, "Customer", "UTF-8"));
+            message_R.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiverMail, "Bugproducer", "UTF-8"));
+            message_R.setSubject("Message from Customer", "UTF-8");
+            message_R.setContent(user_input, "text/html;charset=UTF-8");
+            message_R.setSentDate(new Date());
+            message_R.saveChanges();
+            return message_R;
+        } catch (Exception ignored) {
             return new MimeMessage(session_R);
         }
 
@@ -45,13 +49,76 @@ public class SendMailUseCase {
 
     /**
      * This method is used for sending an email to receiverAccount from senderAccount.
-     * @param user_input The user's input, which is used as the content of th email.
-     * @param senderAccount The email address of the sender.
+     *
+     * @param user_input      The user's input, which is used as the content of th email.
+     * @param senderAccount   The email address of the sender.
      * @param receiverAccount The email address of the receiver.
      */
 
-    public static void send_mail_customer(String user_input, String senderAccount, String receiverAccount){
-        try{
+    public static void send_mail_customer(String user_input, String senderAccount, String receiverAccount) {
+        try {
+            Session session = helper();
+
+            MimeMessage message = createMail_customer(session, senderAccount, receiverAccount, user_input);
+
+            Transport transport = session.getTransport();
+            transport.connect(senderAccount, "Bugproducer123");
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        } catch (Exception ignored) {
+        }
+    }
+
+
+    /**
+     * This method is used for creating an email with sender and receiver. Specifically the sender account for admin is
+     * "bugproducer207@gmail.com" and its password is "Bugproducer123". The receiver account is typed by the user.
+     *
+     * @param session_R    Session object for this email, which represents a mail session.
+     * @param senderMail   The email address of the sender.
+     * @param receiverMail The email address of the receiver.
+     * @param user_input   The user's input, which is used as the content of th email.
+     * @return message_R an email with needed information.
+     */
+
+    public static MimeMessage createMail_admin(Session session_R, String senderMail, String receiverMail, String user_input) {
+        MimeMessage message_R = new MimeMessage(session_R);
+        try {
+            message_R.setFrom(new InternetAddress(senderMail, "Bugproducer", "UTF-8"));
+            message_R.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiverMail, "Customer", "UTF-8"));
+            message_R.setSubject("Message from Admin", "UTF-8");
+            message_R.setContent(user_input, "text/html;charset=UTF-8");
+            message_R.setSentDate(new Date());
+            message_R.saveChanges();
+        } catch (Exception ignored) {
+        }
+        return message_R;
+    }
+
+
+    /**
+     * This method is used for sending an email to receiverAccount from senderAccount.
+     *
+     * @param user_input      The user's input, which is used as the content of th email.
+     * @param senderAccount   The email address of the sender.
+     * @param receiverAccount The email address of the receiver.
+     */
+
+    public static void send_mail_admin(String user_input, String senderAccount, String receiverAccount) {
+        try {
+            Session session = helper();
+
+            MimeMessage message = createMail_admin(session, senderAccount, receiverAccount, user_input);
+
+            Transport transport = session.getTransport();
+            transport.connect(senderAccount, "Bugproducer123");
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        } catch (Exception ignored) {
+        }
+    }
+
+    public static Session helper() {
         Properties props = new Properties();
         props.setProperty("mail.transport.protocol", "smtp");
         props.setProperty("mail.smtp.host", SMTPHost);
@@ -66,70 +133,7 @@ public class SendMailUseCase {
 
         Session session = Session.getInstance(props);
         session.setDebug(true);
-
-        MimeMessage message = createMail_customer(session, senderAccount, receiverAccount, user_input);
-
-        Transport transport = session.getTransport();
-        transport.connect(receiverAccount, "Bugproducer123");
-        transport.sendMessage(message, message.getAllRecipients());
-        transport.close();}
-        catch(Exception ignored){}
+        return session;
     }
 
-
-    /**
-     * This method is used for creating an email with sender and receiver. Specifically the sender account for admin is
-     * "bugproducer207@gmail.com" and its password is "Bugproducer123". The receiver account is typed by the user.
-     * @param session_R Session object for this email, which represents a mail session.
-     * @param senderMail The email address of the sender.
-     * @param receiverMail The email address of the receiver.
-     * @param user_input The user's input, which is used as the content of th email.
-     * @return message_R an email with needed information.
-     */
-
-    public static MimeMessage createMail_admin(Session session_R, String senderMail, String receiverMail, String user_input) {
-        MimeMessage message_R = new MimeMessage(session_R);
-        try{
-            message_R.setFrom(new InternetAddress(senderMail, "Bugproducer", "UTF-8"));
-            message_R.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiverMail, "Customer", "UTF-8"));
-            message_R.setSubject("Message from Admin", "UTF-8");
-            message_R.setContent(user_input, "text/html;charset=UTF-8");
-            message_R.setSentDate(new Date());
-            message_R.saveChanges();}
-        catch(Exception ignored){}
-        return message_R;
-    }
-
-
-    /**
-     * This method is used for sending an email to receiverAccount from senderAccount.
-     * @param user_input The user's input, which is used as the content of th email.
-     * @param senderAccount The email address of the sender.
-     * @param receiverAccount The email address of the receiver.
-     */
-
-    public static void send_mail_admin(String user_input, String senderAccount, String receiverAccount)  {
-        try{Properties props = new Properties();
-            props.setProperty("mail.transport.protocol", "smtp");
-            props.setProperty("mail.smtp.host", SMTPHost);
-            props.setProperty("mail.smtp.auth", "true");
-
-
-            final String smtpPort = "465";
-            props.setProperty("mail.smtp.port", smtpPort);
-            props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-            props.setProperty("mail.smtp.socketFactory.fallback", "false");
-            props.setProperty("mail.smtp.socketFactory.port", smtpPort);
-
-            Session session = Session.getInstance(props);
-            session.setDebug(true);
-
-            MimeMessage message = createMail_admin(session, senderAccount, receiverAccount, user_input);
-
-            Transport transport = session.getTransport();
-            transport.connect(senderAccount, "Bugproducer123");
-            transport.sendMessage(message, message.getAllRecipients());
-            transport.close();}
-        catch(Exception ignored){}
-    }
 }
