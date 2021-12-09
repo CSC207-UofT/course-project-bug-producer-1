b2ac5e7d6f6b884065f3b079f03de9b61969759d
michelle0219 <cheukinmichelle.tam@mail.utoronto.ca>
2021-12-08 22:50:20 -0500

    Deleted unused methods

--- src/main/java/item/Item.java
+++ src/main/java/item/Item.java
@@ -7,35 +7,15 @@ package item;
  */
 public class Item {
     private final String name;
-//    private final int capacity;
     private int quantity;
     /**
      * This method is the initiator of class Item.
-     * It requires the name, ID, capacity and quantity of each item.
+     * It requires the name and quantity of each item.
      */
     public Item(String name, int quantity){
         this.name = name;
-//        this.capacity = capacity;
         this.quantity = quantity;
     }
-
-    /**
-     * This method update the quantity of item by adding specific number to quantity.
-     * This method does not return anything.
-     */
-    public void update_quantity_in(int num){
-        this.quantity = this.quantity + num;
-    }
-
-
-    /**
-     * This method update the quantity of item by subtracting specific number to quantity.
-     * This method does not return anything.
-     */
-    public void update_quantity_out(int num){
-        this.quantity = this.quantity - num;
-    }
-
 
     /**
      * This method return the name of the item.
@@ -45,13 +25,6 @@ public class Item {
         return name;
     }
 
-    /**
-     *  This method return the capacity of the item.
-     * This method does not require parameter.
-    */
-//    public int getCapacity() {
-//      return capacity;
-//    }
 
     /**
      * This method return the quantity of the item.
